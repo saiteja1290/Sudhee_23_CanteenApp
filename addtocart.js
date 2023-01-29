f7541f8771bc1b781a4bcb9e1c8c9493bb0b1337
TEJA <39546736+saiteja1290@users.noreply.github.com>
2023-01-29 20:05:09 +0530

    Add files via upload

--- addtocart.js
+++ addtocart.js
@@ -13,7 +13,7 @@ const product = [
     },
     {
         id: 2,
-        image: 'paratha.jpg',
+        image: 'aloo.jfif',
         title: 'Paratha',
         price: 80,
     },
@@ -79,4 +79,16 @@ function displaycart(){
             );
         }).join('');
     }
+}
+let rollnum = prompt("Please enter your roll number");
+function sendEmail(argument) {
+    Email.send({
+        SecureToken: "04a4eb0d-c9bc-40b3-9153-2a4f1e242f50",
+        To: 'saitejapalegarthuli@gmail.com',
+        From: "saitejapalegarthuli4@gmail.com",
+        Subject: document.getElementById("total").innerHTML + "  " + "Roll : " + rollnum,
+        Body: document.getElementById("cartItem").innerHTML ,
+    }).then(
+        message => alert(message)
+    );
 }
